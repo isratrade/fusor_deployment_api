fe2cd3b77a4a905dbd8c665a15d9c50206947db8
Joseph Magen <joseph@isratrade.co.il>
2015-02-09 15:20:43 +0200

    controller actions

--- app/controllers/api/v2/fusor_deployments_controller.rb
+++ app/controllers/api/v2/fusor_deployments_controller.rb
@@ -14,6 +14,33 @@ module Api
         render :json => @fusor_deployment, :serializer => FusorDeploymentSerializer
       end
 
+      def create
+        @fusor_deployment = FusorDeployment.new(params[:fusor_deployment])
+        if @fusor_deployment.save
+          render :json => @fusor_deployment, :serializer => FusorDeploymentSerializer
+        else
+          render json: {errors: @fusor_deployment.errors}, status: 422
+        end
+      end
+
+      def update
+        @fusor_deployment = FusorDeployment.find(params[:id])
+        if @fusor_deployment.update_attributes(params[:deployment])
+          render :json => @fusor_deployment, :serializer => FusorDeploymentSerializer
+        else
+          render json: {errors: @fusor_deployment.errors}, status: 422
+        end
+      end
+
+      def destroy
+        @fusor_deployment = FusorDeployment.find(params[:id])
+        if @fusor_deployment.destroy
+          render :json => {}, status: 204
+        else
+          render json: {errors: @fusor_deployment.errors}, status: 422
+        end
+      end
+
     end
   end
 end
